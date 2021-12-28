a837d2c346f8c4470619af3f9ab7769521968061
Yash Saraf <51951502+Yashsaraf96@users.noreply.github.com>
2021-12-28 12:37:31 +0530

    stuct
    Basic code for learning struct.

---
+++ structs/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zip   int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email: "jim.party@gmail.com",
+			zip:   940000,
+		},
+	}
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
